3e67a79f5aae57914b2b633e741170e7e0f265fe
Eric Wolfe <rojowolf21@gmail.com>
2017-06-16 12:14:12 -0500

    Working on relations

--- project3/database.sql
+++ project3/database.sql
@@ -3,10 +3,10 @@ CREATE DATABASE mydatabase;
 USE mydatabase;
 
 CREATE TABLE `Employee` (
-  `id`    INT UNSIGNED NOT NULL AUTO_INCREMENT,
-  `name`  VARCHAR(100),
-  `phoneNumber` VARCHAR(10),
-  `DateOfBirth` DATE,
+  `id`          INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `name`        VARCHAR(100) NOT NULL,
+  `phoneNumber` VARCHAR(10) NOT NULL,
+  `dateOfBirth` DATE NOT NULL,
   PRIMARY KEY (`id`)
 );
 
@@ -73,6 +73,100 @@ CREATE TABLE `HospitalOverallRating` (
 );
 
 CREATE TABLE `Hospital`(
+  `id`                        INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `name`                      VARCHAR(100) NOT NULL,
+  `phoneNumber`               VARCHAR(10) NOT NULL,
+  `address`                   VARCHAR(50) NOT NULL,
+  `xCoordinate`               DOUBLE NOT NULL,
+  `yCoordinate`               DOUBLE NOT NULL,
+  `ES`                        BOOL NOT NULL,
+  `EHS`                       BOOL NOT NULL,
+  `City$id`                   INT UNSIGNED NOT NULL,
+  `State$id`                  INT UNSIGNED NOT NULL,
+  `Zipcode$id`                INT UNSIGNED NOT NULL,
+  `County$id`                 INT UNSIGNED NOT NULL,
+  `HospitalType$id`           INT UNSIGNED NOT NULL,
+  `HospitalOwnership$id`      INT UNSIGNED NOT NULL,
+  `HospitalOverallRating$id`  INT UNSIGNED NOT NULL,
+  PRIMARY KEY                 (`id`),
+  FOREIGN KEY                 (`City$id`) REFERENCES `City` (`id`),
+  FOREIGN KEY                 (`State$id`) REFERENCES `State` (`id`),
+  FOREIGN KEY                 (`Zipcode$id`) REFERENCES `Zipcode` (`id`),
+  FOREIGN KEY                 (`County$id`) REFERENCES `County` (`id`),
+  FOREIGN KEY                 (`HospitalType$id`) REFERENCES `HospitalType` (`id`),
+  FOREIGN KEY                 (`HospitalOwnership$id`) REFERENCES `HospitalOwnership` (`id`),
+  FOREIGN KEY                 (`HospitalOverallRating$id`) REFERENCES `HospitalOverallRating` (`id`)
+);
+
+CREATE TABLE `Footnote`(
+  `id`          INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `note`        VARCHAR(500) NOT NULL,
+  PRIMARY KEY   (`id`)
+);
+
+CREATE TABLE `NationalComparison`(
+  `id`            INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `description`   VARCHAR(100) NOT NULL,
+  `Footnote$id`   INT UNSIGNED NULL,
+  PRIMARY KEY     (`id`),
+  FOREIGN KEY     (`Footnote$id`) REFERENCES `Footnote` (`id`)
+);
+
+CREATE TABLE `Mortality` (
+  `NationalComparison$id` INT UNSIGNED NOT NULL,
+  PRIMARY KEY   (`NationalComparison$id`),
+  FOREIGN KEY   (`NationalComparison$id`) REFERENCES  `NationalComparison` (`id`)
+);
+
+CREATE TABLE `SafetyOfCare` (
+  `NationalComparison$id` INT UNSIGNED NOT NULL,
+  PRIMARY KEY   (`NationalComparison$id`),
+  FOREIGN KEY   (`NationalComparison$id`) REFERENCES  `NationalComparison` (`id`)
+);
+
+CREATE TABLE `Readmission` (
+  `NationalComparison$id` INT UNSIGNED NOT NULL,
+  PRIMARY KEY   (`NationalComparison$id`),
+  FOREIGN KEY   (`NationalComparison$id`) REFERENCES  `NationalComparison` (`id`)
+);
+
+CREATE TABLE `PatientExperience` (
+  `NationalComparison$id` INT UNSIGNED NOT NULL,
+  PRIMARY KEY   (`NationalComparison$id`),
+  FOREIGN KEY   (`NationalComparison$id`) REFERENCES  `NationalComparison` (`id`)
+);
+
+CREATE TABLE `EffectivenessOfCare` (
+  `NationalComparison$id` INT UNSIGNED NOT NULL,
+  PRIMARY KEY   (`NationalComparison$id`),
+  FOREIGN KEY   (`NationalComparison$id`) REFERENCES  `NationalComparison` (`id`)
+);
+
+CREATE TABLE `TimelinessOfCare` (
+  `NationalComparison$id` INT UNSIGNED NOT NULL,
+  PRIMARY KEY   (`NationalComparison$id`),
+  FOREIGN KEY   (`NationalComparison$id`) REFERENCES  `NationalComparison` (`id`)
+);
+
+CREATE TABLE `EfficientUseOfMedicalImaging` (
+  `NationalComparison$id` INT UNSIGNED NOT NULL,
+  PRIMARY KEY   (`NationalComparison$id`),
+  FOREIGN KEY   (`NationalComparison$id`) REFERENCES  `NationalComparison` (`id`)
+);
+
+CREATE TABLE `Rating`(
+  `Hospital$id`                       INT UNSIGNED NOT NULL,
+  `Mortality$id`                      INT UNSIGNED NOT NULL,
+  `SafetyOfCare$id`                   INT UNSIGNED NOT NULL,
+  `Readmission$id`                    INT UNSIGNED NOT NULL,
+  `PatientExperience$id`              INT UNSIGNED NOT NULL,
+  `EffectivenessOfCare$id`            INT UNSIGNED NOT NULL,
+  `TimelinessOfCare$id`               INT UNSIGNED NOT NULL,
+  `EfficientUseOfMedicalImaging$id`   INT UNSIGNED NOT NULL,
+  PRIMARY KEY (`Hospital$id`, `Mortality$id`, `SafetyOfCare$id`, `Readmission$id`,
+  `PatientExperience$id`, `EffectivenessOfCare$id`, `TimelinessOfCare$id`, `EfficientUseOfMedicalImaging$id`),
+  FOREIGN KEY (`Hospital$id`) REFERENCES `Hospital` (`id`),
+  
 
 );
 
